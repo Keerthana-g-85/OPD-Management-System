f49d7c7623e0f3584743eadff33e7247e01634b7
Keerthana G <keerthana@CELSYSBLR000337.local>
2026-08-10 15:59:41 +0530

    using transcations

--- server/Service/DoctorService.ts
+++ server/Service/DoctorService.ts
@@ -66,49 +66,46 @@ export default class DoctorService {
     status,
   }: CreateDoctorArguments) {
     try {
-      console.log(
-        name,
-        email,
-        age,
-        gender,
-        address,
-        phone,
-        password,
-        role,
-        image,
-        department,
-        qualification,
-        experience,
-        charges,
-      );
+      return await database.transaction(async (manager) => {
+        const userRepo = manager.getRepository(Users);
+        const doctorRepo = manager.getRepository(Doctor);
+        const departmentRepo = manager.getRepository(Department);
 
-      const depeartmentRepo = database.getRepository(Department);
-      const userEmail = await this.usersRepo.findOneBy({ email: email });
-      if (userEmail) {
-        throw new GraphQLError("User already present");
-      }
-      const salt = bcrypt.genSaltSync(10);
-      const hashPassword = await bcrypt.hash(password, salt);
-      const user = this.usersRepo.create({
-        name,
-        email,
-        age,
-        gender,
-        address,
-        phone,
-        password: hashPassword,
-        role,
-        image,
-        status,
-      });
+        const userEmail = await userRepo.findOneBy({
+          email: email,
+        });
 
-      const userId = await this.usersRepo.save(user);
-      const departmentId = await depeartmentRepo.findOneBy({ id: department });
-      if (!departmentId) {
-        throw new GraphQLError("Department not found");
-      }
-      if (userId) {
-        const doctors = this.doctorRepo.create({
+        if (userEmail) {
+          throw new GraphQLError("User already present");
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const user = this.usersRepo.create({
+          name,
+          email,
+          age,
+          gender,
+          address,
+          phone,
+          password: hashPassword,
+          role,
+          image,
+          status,
+        });
+
+        const userId = await userRepo.save(user);
+
+        const departmentId = await departmentRepo.findOneBy({
+          id: department,
+        });
+
+        if (!departmentId) {
+          throw new GraphQLError("Department not found");
+        }
+
+        const doctors = doctorRepo.create({
           users: userId,
           department: departmentId,
           qualification,
@@ -116,12 +113,13 @@ export default class DoctorService {
           charges,
         });
 
-        await this.doctorRepo.save(doctors);
-      }
-      return {
-        success: true,
-        message: "Doctor successfully added",
-      };
+        await doctorRepo.save(doctors);
+
+        return {
+          success: true,
+          message: "Doctor successfully added",
+        };
+      });
     } catch (error) {
       console.log(error);
       throw new GraphQLError("Error while creating the doctors");
@@ -130,46 +128,50 @@ export default class DoctorService {
 
   async updateDoctor(input: UpdateDoctorArguments) {
     try {
-      const user = await this.usersRepo.findOneBy({ id: input.id });
-      if (!user) {
-        throw new GraphQLError("Doctor not present");
-      }
-      const userInput = {
-        name: input.name || user.name,
-        age: input.age || user.age,
-        email: input.email || user.email,
-        gender: input.gender || user.gender,
-        address: input.address || user.address,
-        phone: input.phone || user.phone,
-        image: input.image || user.image,
-        status: input.status ?? user.status,
-      };
-      const userData: Users = {
-        ...user,
-        ...userInput,
-      };
-      if (userData) {
-        await this.usersRepo.update({ id: input.id }, { ...userData });
-      }
-      const doctor = await this.doctorRepo.findOne({
-        where: { users: { id: user.id } },
-      });
-      console.log(doctor);
-      if (doctor) {
-        const doctorInput = {
-          qualification: input.qualification || doctor.qualification,
-          experience: input.experience || doctor.experience,
-          charges: input.charges || doctor.charges,
+      return await database.transaction(async (manager) => {
+        const usersRepo = manager.getRepository(Users);
+        const doctorRepo = manager.getRepository(Doctor);
+        const user = await usersRepo.findOneBy({ id: input.id });
+        if (!user) {
+          throw new GraphQLError("Doctor not present");
+        }
+        const userInput = {
+          name: input.name || user.name,
+          age: input.age || user.age,
+          email: input.email || user.email,
+          gender: input.gender || user.gender,
+          address: input.address || user.address,
+          phone: input.phone || user.phone,
+          image: input.image || user.image,
+          status: input.status ?? user.status,
         };
-        console.log(input);
-        await this.doctorRepo.update({ id: doctor.id }, doctorInput);
-      } else {
-        console.log("No doctor");
-      }
-      return {
-        success: true,
-        message: "Doctor updated",
-      };
+        const userData: Users = {
+          ...user,
+          ...userInput,
+        };
+        if (userData) {
+          await usersRepo.update({ id: input.id }, { ...userData });
+        }
+        const doctor = await doctorRepo.findOne({
+          where: { users: { id: user.id } },
+        });
+        console.log(doctor);
+        if (doctor) {
+          const doctorInput = {
+            qualification: input.qualification || doctor.qualification,
+            experience: input.experience || doctor.experience,
+            charges: input.charges || doctor.charges,
+          };
+          console.log(input);
+          await doctorRepo.update({ id: doctor.id }, doctorInput);
+        } else {
+          console.log("No doctor");
+        }
+        return {
+          success: true,
+          message: "Doctor updated",
+        };
+      });
     } catch (error) {
       console.log(error);
       throw new GraphQLError("Error while updating doctors ");
